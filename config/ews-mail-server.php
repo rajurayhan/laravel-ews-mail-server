7ab3b8376e6038e0a6cdd55bd23b600c6bbad624
Raju Rayhan <devraju.bd@gmail.com>
2026-09-08 09:44:06 +0600

    Remove v1 EWS config file

--- config/ews-mail-server.php
+++
@@ -1,9 +0,0 @@
-<?php
-    use \jamesiarmes\PhpEws\Client;
-    
-    return [
-        'host'      => 'your_host',
-        'username'  => 'your_email',
-        'password'  => 'your_password',
-        'version'   => Client::VERSION_2016
-    ];
